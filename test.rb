3fe7a841996559b454b6575727bcbfc3e55c7219
Kirill Shaplyko <k.shaplyko@gmail.com>
2014-04-24 22:15:26 +0300

    Let's try multiline comments now

--- test.rb
+++ test.rb
@@ -1,3 +1,11 @@
 # it will be ruby file
 # @Baltazore should know about it
 # new comment here
+# Multiple line comments now
+# @Baltazore did a lot of work
+# to do this shit
+#
+
+# Also @oivoodoo thanks for
+# inspiration to do this
+
