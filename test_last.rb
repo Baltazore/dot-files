85afd88da16c5cd333e0f41968b665160367dad4
Kirill Shaplyko <k.shaplyko@gmail.com>
2014-08-05 20:52:39 +0300

    Last test before specs

---
+++ test_last.rb
@@ -0,0 +1,14 @@
+# @Baltazore is trying to check first line comment
+def super_method(such_args)
+ "Doge tutorial"
+end
+# doing some stuff
+"hello world"
+# @baltazore, Let's check multiline comments,
+# them really rocks
+#
+
+# TODO:
+# - [ ] This Github flavoured markdown
+# - [ ] such nice
+# - [ ] very exciting
