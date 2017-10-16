e09c2dfa7bf8731e2b125afcc55fb0c77334b11f
Kirill Shaplyko <kirillshaplyko@Kirills-MacBook-Pro.local>
2017-10-16 16:32:16 +0300

    Remove old installs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-ln -vnfs "`pwd`/tmux.conf" ~/.tmux.conf
 ln -vnfs "`pwd`/zshrc" ~/.zshrc
-ln -vnfs "`pwd`/vim" ~/.vim
-ln -vnfs "`pwd`/vimrc" ~/.vimrc
-ln -vnfs "`pwd`/thymerc" ~/.thymerc
 ln -vnfs "`pwd`/spacemacs" ~/.spacemacs
